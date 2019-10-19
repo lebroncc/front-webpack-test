dc58ed13f2c76277e713c5b2bde3b67d017bc205
chenchaofan <chen_chaofan@hotmail.com>
2019-10-19 23:32:37 +0800

    login page

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,10 @@ export default class App extends Component{
   render() {
     return (
       <div>
-        my react webpack starter
+        {/* my react webpack starter */}
+        <input type="text" style={{display: 'block'}} placeholder="请输入用户名" />
+        <input type="text" style={{display: 'block', marginTop: 10}} placeholder="请输入密码" />
+        <input type="button" value="确定" />
       </div>
     )
   }
